ba9e299928cf99f9bb7ae51e07938d6461d6d49e
Sumen Tse <sumen.tse@gmail.com>
2023-03-31 11:25:44 -0400

    test yarn

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Testing deploy
+          Testing deploy with yarn
         </p>
         <a
           className="App-link"
